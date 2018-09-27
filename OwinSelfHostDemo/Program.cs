0c4ad98fe2305d03bcbc5dc1161d7f82acbe296b
Jeremy Wells <jwells@vertafore.com>
2018-09-27 15:35:58 -0500

    init

--- OwinSelfHostDemo/Program.cs
+++ OwinSelfHostDemo/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
 
 namespace OwinSelfHostDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string baseAddress = "http://localhost:9000/";
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/Values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ OwinSelfHostDemo/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OwinSelfHostDemo
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "my first amazing value", "another incredible value" };
+        }
+
+        public string Get(int id)
+        {
+            return "the lonely single value";
+        }
+
+        public void Post([FromBody]string value) { }
+
+        public void Put(int id, [FromBody] string value) { }
+
+        public void Delete(int id) { }
+
+    }
+}
+
